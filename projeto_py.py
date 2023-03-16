7003b1abec8596be622b62bdaf2e460412c0ef19
Laryssa Souza <laarylaryy@gmail.com>
2023-03-16 16:57:23 -0300

    cálculo de Idades

---
+++ projeto_py.py
@@ -0,0 +1,8 @@
+nascimento = 2006
+nome = 'Laryssa'
+anoatual = 2023
+def calculoidade(nascimento,anoatual):
+    idade = anoatual - nascimento
+    return idade
+idade = calculoidade(nascimento,anoatual)
+print('idade é', idade)
